06fede9a2df4ba1d33ba9393718508adc1bd8fa8
Julio Santaman <53060728+juliocsantaman@users.noreply.github.com>
2024-06-07 15:36:30 -0500

    Styles, component home and labs

---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.scss'
+})
+export class LabsComponent {
+  title = 'todoapp';
+  tasks = ['Install angular', 'Create components', 'Run app'];
+}
