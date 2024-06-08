212b3aed4dd776e3db6842691f945c2669acf55c
Julio Santaman <53060728+juliocsantaman@users.noreply.github.com>
2024-06-08 15:08:29 -0500

    Events

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,15 +1,35 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.scss'
 })
 export class LabsComponent {
   title = 'todoapp';
   tasks = ['Install angular', 'Create components', 'Run app'];
+  name: string = "Julio Cesar";
+
+  showAlert(text: string): void {
+    alert(text);
+  }
+
+  changeHandler(event: Event): void {
+    console.group('changeHandler');
+    console.log('event', event);
+    console.groupEnd();
+  }
+
+  keydownHandler(event: KeyboardEvent): void {
+    const input = event.target as HTMLInputElement;
+    console.group('keydownHandler');
+    console.log('event', event);
+    console.log(input.value);
+    console.groupEnd();
+  }
 }
