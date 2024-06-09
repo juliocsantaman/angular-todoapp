9560ac42ad3d9d4dd677d9f309da736e0a2536da
Julio Santaman <53060728+juliocsantaman@users.noreply.github.com>
2024-06-08 19:46:10 -0500

    Adding task and deleting using signal

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  tasks = signal(['Install angular', 'Create components', 'Run app']);
+
+  changeHandler(event: Event): void {
+    
+    const input = event.target as HTMLInputElement;
+
+    if(input.value !== '') {
+      const newTask = input.value;
+      this.tasks.update((tasks) => [...tasks, newTask]);
+      input.value = '';
+    }
+
+
+  }
+
+  deleteTask(index: number): void {
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+  }
 
 }
